47baac96ed963226776e947bde48a60d985dea7d
garry329 <jaideep329@gmail.com>
2018-02-10 01:54:49 +0530

    kal ka maal

--- canny-line.py
+++ canny-line.py
@@ -1,7 +1,48 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
 import numpy as np
-import os
+import RPi.GPIO as GPIO
 
+  
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+ 
+Motor1A = 33
+Motor1B = 35
+Motor1E = 37
+Motor2A = 36
+Motor2B = 38
+Motor2E = 40
+
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1E,GPIO.OUT)
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+GPIO.setup(Motor2E,GPIO.OUT)
+
+
+# speeda = GPIO.PWM(Motor1E,100)
+# speedb = GPIO.PWM(Motor2E,100)
+
+# speedb.start(25)#left motor
+# speeda.start(25)#right motor
+GPIO.output(Motor1A,0)
+GPIO.output(Motor1B,0)
+GPIO.output(Motor2A,0)
+GPIO.output(Motor2B,0)
+
+delay_time = 0.3
+thicknessAllowance = 20
+camera = PiCamera()
+camera.resolution = (640,640)
+camera.framerate = 43
+rawCapture = PiRGBArray(camera, size=(640,640))
+
+time.sleep(1)
+font = cv2.FONT_HERSHEY_SIMPLEX
 left = -1
 centre = 0
 right = 1
@@ -78,78 +119,74 @@ def getDecision(cx1,cx2,cx3,cx4,cx5,prev1,prev2):
         return left,diff1,diff2
     else:
         return right,diff1,diff2
-image = cv2.imread("left.jpeg", -1)
-# print image.length
-crop_img1 = image[0:128,0:640]
-crop_img2 = image[128:256,0:640]
-crop_img3 = image[256:384,0:640]
-crop_img4 = image[384:512,0:640]
-crop_img5 = image[512:640,0:640]
-font = cv2.FONT_HERSHEY_SIMPLEX
-cx1,cy1 = getCentre(crop_img1)
-cx2,cy2 = getCentre(crop_img2)
-cx3,cy3 = getCentre(crop_img3)
-cx4,cy4 = getCentre(crop_img4)
-cx5,cy5 = getCentre(crop_img5)
-# edges = cv2.Canny(img,10,150,apertureSize = 3)
+def turn_left():
+    GPIO.output(Motor1A,1)
+    GPIO.output(Motor1B,1)
+    GPIO.output(Motor2A,0)
+    GPIO.output(Motor2B,1)
+    print "turn left"
+def turn_right():
+    GPIO.output(Motor1A,0)
+    GPIO.output(Motor1B,1)
+    GPIO.output(Motor2A,1)
+    GPIO.output(Motor2B,1)
+    print "turn right"
+def go_straight():
+    GPIO.output(Motor1A,0)
+    GPIO.output(Motor1B,1)
+    GPIO.output(Motor2A,0)
+    GPIO.output(Motor2B,1)
+    print "go straight"
+def motor_stop():
+    GPIO.output(Motor1A,0)
+    GPIO.output(Motor1B,0)
+    GPIO.output(Motor2A,0)
+    GPIO.output(Motor2B,0)
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    
+    image = frame.array
+    crop_img1 = image[0:128,0:640]
+    crop_img2 = image[128:256,0:640]
+    crop_img3 = image[256:384,0:640]
+    crop_img4 = image[384:512,0:640]
+    crop_img5 = image[512:640,0:640]
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cx1,cy1 = getCentre(crop_img1)
+    cx2,cy2 = getCentre(crop_img2)
+    cx3,cy3 = getCentre(crop_img3)
+    cx4,cy4 = getCentre(crop_img4)
+    cx5,cy5 = getCentre(crop_img5)
+    # edges = cv2.Canny(img,10,150,apertureSize = 3)
 
-decision , previousDecision1 , previousDecision2 =  getDecision(cx1,cx2,cx3,cx4,cx5,-1,-1)  
+    decision , previousDecision1 , previousDecision2 =  getDecision(cx1,cx2,cx3,cx4,cx5,-1,-1)  
 
-print decision
-# # This returns an array of r and theta values
-# lines = cv2.HoughLines(edges,1,np.pi/180, 110)
- 
-# # The below for loop runs till r and theta values 
-# # are in the range of the 2d array
-# for line in lines:
-# 	for r,theta in line:
-     
-# 		# Stores the value of cos(theta) in a
-# 		a = np.cos(theta)
+    print decision
 
-# 		# Stores the value of sin(theta) in b
-# 		b = np.sin(theta)
-		 
-# 		# x0 stores the value rcos(theta)
-# 		x0 = a*r
-		 
-# 		# y0 stores the value rsin(theta)
-# 		y0 = b*r
-		 
-# 		# x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
-# 		x1 = int(x0 + 1000*(-b))
-		 
-# 		# y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
-# 		y1 = int(y0 + 1000*(a))
-
-# 		# x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
-# 		x2 = int(x0 - 1000*(-b))
-		 
-# 		# y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
-# 		y2 = int(y0 - 1000*(a))
-		 
-# 		# cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
-# 		# (0,0,255) denotes the colour of the line to be 
-# 		#drawn. In this case, it is red. 
-# 		cv2.line(img,(x1,y1), (x2,y2), (0,0,255),2)
-
-while True:
+    if decision == centre:
+        go_straight()
+    elif decision == left:
+        turn_left()
+    else:
+        turn_right()
+    while True:
     #cv2.imshow('frame',image)
-    cv2.putText(crop_img1,'CX:'+str(cx1), (15,30),font,1,(0,0,255),2)
-    cv2.putText(crop_img1,'CY: '+str(cy1),(15,70),font,1,(0,0,255),2)
-    cv2.putText(crop_img2,'CX:'+str(cx2), (15,30),font,1,(0,0,255),2)
-    cv2.putText(crop_img2,'CY: '+str(cy2),(15,70),font,1,(0,0,255),2)
-    cv2.putText(crop_img3,'CX:'+str(cx3), (15,30),font,1,(0,0,255),2)
-    cv2.putText(crop_img3,'CY: '+str(cy3),(15,70),font,1,(0,0,255),2)
-    cv2.putText(crop_img4,'CX:'+str(cx4), (15,30),font,1,(0,0,255),2)
-    cv2.putText(crop_img4,'CY: '+str(cy4),(15,70),font,1,(0,0,255),2)
-    cv2.putText(crop_img5,'CX:'+str(cx5), (15,30),font,1,(0,0,255),2)
-    cv2.putText(crop_img5,'CY: '+str(cy5),(15,70),font,1,(0,0,255),2)
-    cv2.imshow('Frame',image)
-    # cv2.imshow('Frame 1',crop_img1)
-    # cv2.imshow('Frame 2',crop_img2)
-    # cv2.imshow('Frame 3',crop_img3)
-    # cv2.imshow('Frame 4',crop_img4)
-    # cv2.imshow('Frame 5',crop_img5)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-    	break
+        cv2.putText(crop_img1,'CX:'+str(cx1), (15,30),font,1,(0,0,255),2)
+        cv2.putText(crop_img1,'CY: '+str(cy1),(15,70),font,1,(0,0,255),2)
+        cv2.putText(crop_img2,'CX:'+str(cx2), (15,30),font,1,(0,0,255),2)
+        cv2.putText(crop_img2,'CY: '+str(cy2),(15,70),font,1,(0,0,255),2)
+        cv2.putText(crop_img3,'CX:'+str(cx3), (15,30),font,1,(0,0,255),2)
+        cv2.putText(crop_img3,'CY: '+str(cy3),(15,70),font,1,(0,0,255),2)
+        cv2.putText(crop_img4,'CX:'+str(cx4), (15,30),font,1,(0,0,255),2)
+        cv2.putText(crop_img4,'CY: '+str(cy4),(15,70),font,1,(0,0,255),2)
+        cv2.putText(crop_img5,'CX:'+str(cx5), (15,30),font,1,(0,0,255),2)
+        cv2.putText(crop_img5,'CY: '+str(cy5),(15,70),font,1,(0,0,255),2)
+        cv2.imshow('Frame',image)
+        # cv2.imshow('Frame 1',crop_img1)
+        # cv2.imshow('Frame 2',crop_img2)
+        # cv2.imshow('Frame 3',crop_img3)
+        # cv2.imshow('Frame 4',crop_img4)
+        # cv2.imshow('Frame 5',crop_img5)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break                    
+     
+GPIO.cleanup()
