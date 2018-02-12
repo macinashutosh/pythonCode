4e0b1b0703b15492cb12387246a62de79af403e8
danytargaryen <itssmartashutosh@gmail.com>
2018-02-13 03:28:20 +0530

    changes-bone.py

--- bone.py
+++ bone.py
@@ -74,9 +74,9 @@ def motor_stop():
 
 
 camera = PiCamera()
-camera.resolution = (160, 480)
+camera.resolution = (160, 600)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(160, 480))
+rawCapture = PiRGBArray(camera, size=(160, 600))
 time.sleep(1)
 current_dec = 0 #1 for Right -1 for Left
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -85,6 +85,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
  crop_img1 = image[120:240,0:160]
  crop_img2 = image[240:360,0:160]
  crop_img3 = image[360:480,0:160]
+ crop_img4 = image[480:600,0:160]
 
     # Convert to grayscale
  gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
@@ -146,12 +147,31 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     # Find the contours of the frame
  img, contours3, hierarchy = cv2.findContours(mask.copy(), 1, cv2.CHAIN_APPROX_NONE)
+ 
+ gray = cv2.cvtColor(crop_img4, cv2.COLOR_BGR2GRAY)
+ blur = cv2.GaussianBlur(gray,(5,5),0)
+
+    # Color thresholding
+ ret,thresh1 = cv2.threshold(blur,60,255,cv2.THRESH_BINARY_INV)
+
+    # Erode and dilate to remove accidental line detections
+ mask = cv2.erode(thresh1, None, iterations=2)
+ mask = cv2.dilate(mask, None, iterations=2)
+
+    # Find the contours of the frame
+ img, contours4, hierarchy = cv2.findContours(mask.copy(), 1, cv2.CHAIN_APPROX_NONE)
     # Find the biggest contour (if detected)
+ delay_time = 0.3
  final_contours = contours1
  if len(contours2)>0:
   final_contours = contours2
+  delay_time = 0.4
  elif len(contours3)>0:
   final_contours = contours3
+  delay_time = 0.5
+ elif len(contours4)>0:
+  final_contours = contours4
+  delay_time = 0.6
   
   
  if len(contours) > 0:
@@ -204,25 +224,23 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
   #print cx
   #print cy
 
-  if cx >= 120: #right
+  if cx > 75: #right
    offset_x = (cx-60)
    print offset_x
    turn_right()
    current_dec = 1
    #GPIO.output(40, GPIO.HIGH)
    #GPIO.output(35, GPIO.LOW)
-  if cx < 120 and cx > 50:
-   go_straight()
    #GPIO.output(40, GPIO.LOW)
    #GPIO.output(35, GPIO.LOW)
-  if cx <= 50: #left
+  if cx <= 75: #left
    offset_x = (60-(60-cx))
    print offset_x
    turn_left()
    current_dec = -1
    #GPIO.output(40, GPIO.LOW) 
    #GPIO.output(35, GPIO.HIGH)
-  time.sleep(0.3)
+  time.sleep(delay_time)
   motor_stop()
  else:
   if current_dec == 1:
@@ -236,7 +254,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 
     #Display the resulting frame
- #cv2.imshow('frame',crop_img)
+ #cv2.imshow('frame',crop_img3)
+ #cv2.imshow('frame1',crop_img2)
  rawCapture.truncate(0)
  if cv2.waitKey(1) & 0xFF == ord('q'):
   break
